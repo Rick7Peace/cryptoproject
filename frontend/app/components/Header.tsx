0311f1a5a941c4921c65138101ac225d0df247df
palthol <ilikericeat@gmail.com>
2025-04-03 19:56:51 -0400

    Removed obselete files, changed imports for header and prepared one of our pages.

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import './Header.css'; // Assuming you have a CSS file for styling
 
 const Header = () => {
